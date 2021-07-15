0bbb498d030416f56f0fce3db091c5d117d153f7
xbw1220 <a18570550499@163.com>
2021-07-15 18:22:28 +0800

    'TabBar导入'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
     <router-view/>
+    <main-tab-bar/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MainTabBar from 'content/mainTabBar/MainTabBar'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'App',
+  components:{
+    MainTabBar
   }
 }
+</script>
+
+<style lang="less">
+ @import "@/assets/css/base.less";
+#app {
+    position: relative;
+}
+
 </style>
