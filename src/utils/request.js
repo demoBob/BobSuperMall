854e88eefda3d0c982ac500d0b47dc67865d3c7f
谢博文 <58241662+demoBob@users.noreply.github.com>
2022-02-16 16:14:08 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const service = axios.create({
-  baseURL:"http://152.136.185.210:7878/api/m5",
+  baseURL:"http://152.136.185.210:7878/api/hy66",
   timeout: 15000
 })
 
